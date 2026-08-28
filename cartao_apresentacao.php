81301a39eca780c104d82b32b9d1967714f3b728
PeSzpak <dropszpalves@gmail.com>
2026-08-27 22:15:31 -0300

    feat: cartao de apresentacao em PHP

---
+++ cartao_apresentacao.php
@@ -0,0 +1,29 @@
+<?php 
+    //cria variavel chamada $nome para armazenar o nome da pessoa
+    $nome = "Vardecir Koschak"; 
+
+    //Cria uma variavel chamada $curso para armazenar o curso 
+    $curso = "Harvard só para baixinhos";
+
+    //Cria uma variavel chamada $diciplina 
+    $diciplina = "binario kids";
+
+    //cria variavel chamada $semestre
+    $semestre = 4;
+
+    //cria variavel chamada $objetivo 
+    $objetivo = "baguga baguga";
+
+    echo "=====================================================" . PHP_EOL;
+
+    echo "               CARTAO DE APRESENTAÇÃO                " . PHP_EOL;
+
+    echo "=====================================================" . PHP_EOL;
+    echo "Nome: {$nome}" . PHP_EOL;
+    echo "curso: {$curso}" . PHP_EOL;
+    echo "diciplina: {$diciplina}" . PHP_EOL;
+    echo "semestre: {$semestre}" . PHP_EOL;
+    echo "objetivo: {$objetivo}" . PHP_EOL;
+    echo "=====================================================" . PHP_EOL;
+
+
